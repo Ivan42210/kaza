475a124ef895dd9681e3dfe6fe864ea5ca876df2
Ivan42210 <ivan.cha.42@outlook.fr>
2023-03-26 19:42:00 +0200

    modifications liens

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,19 @@
-import { Link } from 'react-router-dom';
+
+import { NavLink } from 'react-router-dom';
 import logo from '../../assets/logo.svg'
 import '../../styles/Header.css'
 
 
 function Header(){
+
+  
+
     return(
         <nav className="max-width header">
             <img src={logo} alt="" />
             <div>
-                <Link to="/" className='nav-link link-primary'>Accueil</Link>
-                <Link to="/about" className='nav-link link-primary'>A propos</Link>
+                <NavLink to="/" className={({isActive}) => (isActive ? "nav-link-active" : "nav-link")}>Accueil</NavLink>
+                <NavLink to="/about" className={({isActive}) => (isActive ? "nav-link-active" : "nav-link")}>A propos</NavLink>
             </div>
         </nav>
     )
