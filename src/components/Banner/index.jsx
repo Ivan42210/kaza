1a1e5740419077ee5ec27532930d4ff026d4a86e
Ivan42210 <ivan.cha.42@outlook.fr>
2023-04-06 20:32:18 +0200

    modifications

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,11 +1,15 @@
 function Banner({img, text, alt}){
     return (
+        
         <section className='banner position-relative'>
         <img src={img} alt={alt} />
         <div className='banner-text'>
         <h2 className='text-light'>{text}</h2>
         </div>
+      
     </section>
+     
+     
     )
 }
 
